e4273e3d58f5574c17154da762b5457d556bc402
Takara Truong <70541058+takaratruong@users.noreply.github.com>
2021-04-29 20:32:43 -0700

    finalized version stuff

--- FeatureExtractor_amanda.py
+++ FeatureExtractor_amanda.py
@@ -13,12 +13,41 @@ import matplotlib.pyplot as plt
 
 def feature_extractor(val_1, val_2):
 
+    if val_1 > val_2:
+        temp = val_1
+        val_1 = val_2
+        val_2 = temp
+
+    trail = non_trailing_zero_count(val_1, val_2)
+    tot_digits = total_number_digit(val_1, val_2)
+    dif_digits = digit_diff(val_1, val_2)
+    num_carry = number_of_carry(val_1, val_2)
+    num_zero = number_of_zero(val_1,val_2)
+
+    if trail:
+        num_zero = 'trailed'
+        tot_digits = 'trailed'
+        dif_digits = 'trailed'
+    else:
+        trail = 'trailFalse'
+
+    if int(val_1) == 0 or int(val_2) == 0:
+        num_1_digit = 'baseCase'
+        num_2_digit = 'baseCase'
+        #num_carry = 'baseCase'
+        tot_digits = 'baseCase'
+        dif_digits = 'baseCase'
+        num_zero = 'baseCase'
+
     #feature = (total_number_digit(val_1, val_2), digit_diff(val_1, val_2), is_same_number(val_1, val_2), number_of_carry(val_1, val_2), number_of_zero(val_1, val_2))
     #feature = (number_of_digit(val_1),number_of_digit(val_2), is_same_number(val_1, val_2), number_of_carry(val_1, val_2), number_of_zero(val_1, val_2))
     
-    feature = (total_number_digit(val_1, val_2), digit_diff(val_1, val_2), number_of_carry(val_1, val_2))
+    #feature = (total_number_digit(val_1, val_2), digit_diff(val_1, val_2), number_of_carry(val_1, val_2))
     #feature = (max_digit(val_1,val_2), digit_diff(val_1, val_2), number_of_carry(val_1, val_2))
-    
+
+    feature = (tot_digits, dif_digits, num_carry, num_zero, trail)
+    #feature = (num_1_digit, num_2_digit, num_carry, num_zero)
+
     return feature
 
 def max_digit (x, y) :
@@ -61,10 +90,24 @@ def number_of_zero (x , y) :
     def count_zeros(x):
         return str(x).count('0')
     return count_zeros(x) +count_zeros(y)
-    
+
+def non_trailing_zero_count(val_1, val_2):
+
+    total_zero_1 = str(val_1).count('0')
+    total_zero_2 = str(val_2).count('0')
+
+    num1_str = str(val_1)
+    num2_str = str(val_2)
+
+    trail_1 = len(num1_str) - len(num1_str.rstrip('0'))
+    trail_2 = len(num2_str) - len(num2_str.rstrip('0'))
+
+    return ((len(num1_str) - trail_1) == 1  and  (len(num2_str) - trail_2) == 1  and val_1 != 0 and val_2 != 0 and total_zero_1 != 0 and total_zero_2 != 0)
+
+
     
 def main ():
-    MAX_NUM = 5000
+    MAX_NUM = 999
     bins = defaultdict(lambda: [])
 
     for num_1 in range(0, MAX_NUM + 1):
@@ -81,10 +124,10 @@ def main ():
     for key in bins:
         print("Bin Name: " + str(key) + "   Bin Count: " + str(len(bins[key])))
         x.append(len(bins[key]))
-        #print(bins[key])
+        print(bins[key][0:100])
         print("")
-    # plt.plot(x)
-    # plt.ylabel('some numbers')
-    # plt.show()
+    #plt.plot(x)
+    #plt.ylabel('some numbers')
+    #plt.show()
 
 main()
